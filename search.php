8badf60710c2a90cba40a2d76358de05b77be38c
shravan97 <shravanmurali@gmail.com>
2015-08-18 15:20:44 +0530

    search for groups is now possible!

--- search.php
+++ search.php
@@ -4,18 +4,20 @@ if(!isset($_SESSION['user']))
 	header("Location:log_page.php");
 else{
 	if (empty($_REQUEST['search_query'])) {
-		header("Location:index.php?s_empty=145345fcdfgdfg");
+		header("Location:index.php?s_empty=1");
 
 	}
 	else{
+		$search=$_REQUEST['search_query'];
 		$h=mysqli_connect("localhost","root","","soc_media");
 		$query="select roll_no from stud_details where roll_no like '%".(string)$_REQUEST['search_query']."%';";
 	    $r=mysqli_query($h,$query);
-       
+        $query_g="select id , name from groups where name like '%".$search."%';";
+        $r_g=mysqli_query($h,$query_g) or die(mysqli_error($h));
 echo "<!DOCTYPE html>
 <html>
 <head>
-	<title>search for ".(string)$_REQUEST['search_query']."</title>
+	<title>search for "."'".(string)$_REQUEST['search_query']."'</title>
 </head>
 <body>
 ";
@@ -29,6 +31,13 @@ echo "<!DOCTYPE html>
           	echo "page_generate.php?user=".$arr['roll_no'];
 
 	   	echo "'>".(string)$arr['roll_no']."</a><br/>";
+	   }
+	    while ($arr_g=mysqli_fetch_array($r_g)) 
+	   {
+	   	echo "<a href='";
+        echo "/project/".$arr_g['id'];
+          
+	   	echo "'>".$arr_g['name']."</a><br/>";
 	   }
 	}
 }
